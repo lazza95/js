9c5de26fe649878bd5258f2e58c2a030be21f768
Lazzaretti Luca <76959613+lazza95@users.noreply.github.com>
2022-06-06 17:31:05 +0200

    Add files via upload

---
+++ compito 0606/js/index.js
@@ -0,0 +1,17 @@
+alert('inizia a studiare');
+window.alert('studia di più');
+window.alert('il tempo stringe');
+window.alert('non è abbastanza');
+window.alert('guarda la console');
+
+console.log("clicca sul pulsante");
+
+function funzione(){
+    document.write("Inzio del Test!!!")
+    
+}
+    
+
+
+
+
